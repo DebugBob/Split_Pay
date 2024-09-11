96d961093696c7845f116320860f214373c1741f
DebugBob <rastorino@outlook.com.au>
2024-09-11 23:24:48 +1000

    Added function for calculating 10% from total price

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var totalPrice = document.detElementById("TOTAL PRICE READOUT")
+
+function calcPercentage(totalPrice)
+{
+    return totalPrice * 0.10;
+}
