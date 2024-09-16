10ded8e52ce607327980649a59ec25871c6e244e
RebeccaZhu29 <pingbozhu59@gmail.com>
2024-09-16 20:38:52 +1000

    added logic for back button

---
+++ assets/js/finalpage.js
@@ -0,0 +1,4 @@
+// Click on the "back" button to go back to the first page
+document.getElementById("back-button").addEventListener("click", function () {
+  window.location.href = "Index.html";
+});
